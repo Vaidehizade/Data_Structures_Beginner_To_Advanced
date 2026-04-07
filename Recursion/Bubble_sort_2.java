4106316372fdd949690c8865c30d12aeab321c20
Vaidehizade <vaidehizade2001@gmail.com>
2026-04-07 23:56:12 +0530

    recursive practice

---
+++ Recursion/Bubble_sort_2.java
@@ -0,0 +1,26 @@
+package Recursion;
+
+public class Bubble_sort_2 {
+    public static void bubble_sort_rec(int[] arr, int n, int j){
+       if(n==1){
+          return ;
+       }
+
+       if(j==n-1){
+        bubble_sort_rec(arr, n-1, 0);
+        return;
+       }
+
+       if(arr[j] > arr[j+1]){
+            int temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+        }
+       bubble_sort_rec(arr,n-1,j+1);
+
+    }
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        bubble_sort_rec(arr,5);
+    }
+}
