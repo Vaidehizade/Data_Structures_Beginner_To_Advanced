fddfef97ebdb7f52b1c40e34df732bc6581e69cf
Vaidehizade <vaidehizade2001@gmail.com>
2026-04-10 23:57:21 +0530

    tilling problem solved

--- Recursion/TillingProblem.java
+++ Recursion/TillingProblem.java
@@ -5,13 +5,15 @@ public class TillingProblem {
     public static int countWays(int n, int m) {
 
         // base cases
-        if(n < m) {
-            return 1;   // only vertical placement possible
-        }
+        // if(n < m) {
+        //     return 1;   // only vertical placement possible
+        // }
 
-        if(n == m) {
-            return 2;   // all vertical OR all horizontal
-        }
+        // if(n == m) {
+        //     return 2;   // all vertical OR all horizontal
+        // }
+        if(n==0 || n==1 || n==2 || n==3)
+            return 1;
 
         // recursive relation
         return countWays(n - 1, m) + countWays(n - m, m);
