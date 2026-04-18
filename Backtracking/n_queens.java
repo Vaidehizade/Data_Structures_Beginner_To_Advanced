cb734461fb8fa350f8e1807405236b1bb7636707
Vaidehizade <vaidehizade2001@gmail.com>
2026-04-18 19:55:56 +0530

    n queens solution corrected

--- Backtracking/n_queens.java
+++ Backtracking/n_queens.java
@@ -13,16 +13,15 @@ public class n_queens {
     }
 
     public static boolean canPlace(int[][] board, int n, int x, int y) {
-         for(int i=x; i<n ; i++){
-            for(int j=y; j<n; j++){
-                if(board[i][j] == 1)
+         for(int i=0; i<x ; i++){
+                if(board[i][y] == 1)
                     return false;
             }
-         }
 
          //left diagonal
-         int i=0,j=0;
-        while(i>=n && j>=n){
+         int i=x;
+         int j=y;
+        while(i>=0 && j>=0){
             if(board[i][j] == 1){
                 return false;
             }
@@ -30,9 +29,10 @@ public class n_queens {
         }
 
          //right diagonal
-         int p=0,q=0;
-         while(p>=n && q>=n){
-            if(board[p][q] == 1){
+         i=x;
+         j=y;
+         while(i>=0 && j<n){
+            if(board[i][j] == 1){
                 return false;
             }
             i--; j++;
@@ -66,8 +66,8 @@ public class n_queens {
     }
 
     public static void main(String[] args) {
-        int n = 4;
-        int[][] board = new int[4][4];
+        int n = 5;
+        int[][] board = new int[5][5];
         boolean ans = solveNQueens(board, 0, n);
         System.out.println(ans);
     }
