c11dfd0b2d327f810031c3955b9ad23ffc9e11ec
Vaidehizade <vaidehizade2001@gmail.com>
2026-04-18 15:17:07 +0530

    permutation of strings

---
+++ Backtracking/permutations_of_string.java
@@ -0,0 +1,29 @@
+package Backtracking;
+
+public class permutations_of_string {
+    public static void permute(char[] ans, int i, int len){
+        //base case
+        if(i==len){
+            System.out.println(new String(ans));
+            return;
+        }
+        //recursive case
+         // recursive case: swap current position with all positions from i onwards
+        for(int j=i; j< len; i++){
+            char temp = ans[i];
+            ans[i] = ans[j];
+            ans[j] = temp;
+            permute(ans, i+1, len);
+            //backtrack
+        temp = ans[i];
+        ans[i] = ans[j];
+        ans[j] = temp; 
+
+    }
+}
+    public static void main(String[] args) {
+        String st = "ABC";
+        int n = st.length();
+        permute(st.toCharArray(),0, n);
+    }
+}
